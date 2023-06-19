5c6a31378f406bb40b5d418c0219739d4c4cdc02
Niko Pitkonen <niko.pitkonen@gofore.com>
2023-06-19 16:27:14 +0300

    initial

--- services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/factory/AlluDataFactory.kt
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/factory/AlluDataFactory.kt
@@ -161,7 +161,7 @@ class AlluDataFactory(
             pendingOnClient: Boolean = false,
             workDescription: String = "Work description.",
             customerWithContacts: CustomerWithContacts =
-                createCompanyCustomer().withContacts(createContact()),
+                createCompanyCustomer().withContacts(createContact(orderer = true)),
             contractorWithContacts: CustomerWithContacts =
                 createCompanyCustomer().withContacts(createContact()),
             representativeWithContacts: CustomerWithContacts? = null,
